3e68268221ad9637c8069c5258f5c50addc27b6a
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2022-11-12 10:07:22 +0100

    style: adds styling to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,15 +7,21 @@ import { colors, space } from '../styles/variables';
 const Navbar = () => {
   return (
     <Wrapper>
-      <div>
-        <img src={LogoSvg} alt="" />
+      <div className="logo-container">
+        <img src={LogoSvg} alt="logo" />
       </div>
       <nav>
         <ul>
           <li>
             <Link to="/">Home</Link>
+          </li>
+          <li>
             <Link to="/connect-wallet">Place to stay</Link>
+          </li>
+          <li>
             <Link to="#">NFTs</Link>
+          </li>
+          <li>
             <Link to="#">Community</Link>
           </li>
         </ul>
@@ -33,16 +39,33 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  .logo-container {
+    height: 1.5rem;
+    img{
+      width: 100%;
+      height: 100%;
+    }
+  }
   @media (min-width: 800px) {
-    padding: 2rem  ${space.lgSpacing};
+    padding: 2rem ${space.lgSpacing};
   }
   @media (min-width: 1200px) {
-    padding: 2rem  ${space.xxlSpacing};
+    padding: 2rem ${space.xxlSpacing};
   }
-.btn-contribute{
-  background: ${colors.bg};
-  font-size: inherit;
-}
+  .btn-contribute {
+    background: ${colors.bg};
+    font-size: inherit;
+    color: ${colors.bgAlpha};
+  }
+  nav {
+    color: ${colors.textPry};
+    width: 40%;
+    ul {
+      display: flex;
+      justify-content: space-between;
+      list-style-type: none;
+    }
+  }
 `;
 
 export default Navbar;
--- src/styles/variables.js
+++ src/styles/variables.js
@@ -1,9 +1,10 @@
 export const colors = {
- bg:'linear-gradient(90deg, #A02279 11.45%, #A02279 11.45%)',                                                                                   
-  bgSec: 'rgb(244, 244, 244)',
+  bg: 'linear-gradient(90deg, #A02279 11.45%, #A02279 11.45%)',
+  bgSec: '#1D1D1E',
   bgAlpha: '#FFFFFF',
   red: 'rgb(256, 106, 110)',
-  text: 'rgb(72, 80, 86)',
+  textPry: '#434343',
+  textSec: '#00000',
   primary: 'rgb(43, 73, 94)',
 };
 
