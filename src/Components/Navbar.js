b9117922a8e4d7412d2c1fc81eb3715ea8f606bb
Adebanjo Afolasope <folasopeadebanjo@gmail.com>
2022-11-12 09:51:35 +0100

    style: aligns navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { LogoSvg } from '../assets';
+import { colors, space } from '../styles/variables';
 
 const Navbar = () => {
   return (
@@ -20,15 +21,28 @@ const Navbar = () => {
         </ul>
       </nav>
       <div>
-        <button>Connect Wallet</button>
+        <button className="btn btn-contribute">Connect Wallet</button>
       </div>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
+  border: 1px solid red;
+  padding: 2rem;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  @media (min-width: 800px) {
+    padding: 2rem  ${space.lgSpacing};
+  }
+  @media (min-width: 1200px) {
+    padding: 2rem  ${space.xxlSpacing};
+  }
+.btn-contribute{
+  background: ${colors.bg};
+  font-size: inherit;
+}
 `;
 
 export default Navbar;
---
+++ src/styles/variables.js
@@ -0,0 +1,19 @@
+export const colors = {
+ bg:'linear-gradient(90deg, #A02279 11.45%, #A02279 11.45%)',                                                                                   
+  bgSec: 'rgb(244, 244, 244)',
+  bgAlpha: '#FFFFFF',
+  red: 'rgb(256, 106, 110)',
+  text: 'rgb(72, 80, 86)',
+  primary: 'rgb(43, 73, 94)',
+};
+
+
+export const space = {
+  sidebarWidth: `300px`,
+  smSpacing: `1rem`,
+  mdSpacing: `1.5rem`,
+  lgSpacing: `2rem`,
+  xlSpacing: `3rem`,
+  xxlSpacing: `5rem`,
+  borderRadius: `6px`,
+};
